350d9182b3d9538b9afc487f1e641cd7c9128e3a
natanbr <br.natan@gmail.com>
2015-06-22 19:55:00 +0300

    Solution 1.5

--- Mixter.Domain/Core/Messages/Message.cs
+++ Mixter.Domain/Core/Messages/Message.cs
@@ -30,11 +30,12 @@ namespace Mixter.Domain.Core.Messages
 
         public void Republish(IEventPublisher eventPublisher, UserId republisher)
         {
-            if (!_projection.Publishers.Contains(republisher))
-            {
-                var evt = new MessageRepublished(GetId(), republisher);
-                PublishEvent(eventPublisher, evt);
-            }
+            if (_projection.Publishers.Contains(republisher)) return;
+
+            if (_projection.IsDeleted) return;
+
+            var evt = new MessageRepublished(GetId(), republisher);
+            PublishEvent(eventPublisher, evt);
         }
 
         private void PublishEvent<TEvent>(IEventPublisher eventPublisher, TEvent evt) where TEvent : IDomainEvent
@@ -45,16 +46,11 @@ namespace Mixter.Domain.Core.Messages
 
         public void Reply(IEventPublisher eventPublisher, UserId replier, string replyContent)
         {
-
-           /* if (!_projection.IsDeleted)
+            if (!_projection.IsDeleted)
             {
                 var evt = new ReplyMessagePublished(MessageId.Generate(), replier, replyContent, _projection.Id);
                 eventPublisher.Publish(evt);
-            }*/
-
-            var evt = new ReplyMessagePublished(MessageId.Generate(), replier, replyContent, _projection.Id);
-            eventPublisher.Publish(evt);
-
+            }
         }
 
         public void Delete(IEventPublisher eventPublisher, UserId deleter)
